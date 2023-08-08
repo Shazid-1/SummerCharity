731e02a811cec35cf22d6b9412a265159ac35531
adian57011 <adian57011@gmail.com>
2023-08-09 00:46:24 +0600

    DataFactory-Edited

--- DAL/DataAccessFactory.cs
+++ DAL/DataAccessFactory.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DAL.EF.Models;
+using DAL.Interfaces;
+using DAL.Repos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,38 @@ namespace DAL
 {
     public class DataAccessFactory
     {
+        public static IRepo<Admin,int, bool> AdminData()
+        {
+            return new AdminRepo();
+        }
+
+        public static IRepo<Creator,int, bool> CreatorData()
+        {
+            return new CreatorRepo();
+        }
+
+        public static IRepo<Donator, int, bool> DonatorData()
+        {
+            return new DonatorRepo();
+        }
+
+        public static IRepo<EventDonator, int, bool> EventDonatorData()
+        {
+            return new EventDonatorRepo();
+        }
+
+        public static IRepo<Event, int, bool> EventData()
+        {
+            return new EventRepo();
+        }
+
+        public static IRepo<Membership, int, bool> MembershipData()
+        {
+            return new MembershipRepo();
+        }
+
+
+
+
     }
 }
